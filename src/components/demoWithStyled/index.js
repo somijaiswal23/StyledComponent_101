c5e02a32056b656937f3d07630c3e557e3dd01c3
Somi Jaiswal <42111833+somijaiswal23@users.noreply.github.com>
2020-05-05 23:37:40 +1000

    Update index.js

--- src/components/demoWithStyled/index.js
+++ src/components/demoWithStyled/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+
+
 function DemoWithStyled() {
   return (
     <div>
-      hello from styled components
+         <Button >I am a Button</Button>
+          <Button >I am a Button</Button>
+           <Button >I am a Button</Button>
+           <Button>I am a Button</Button>
+           <h1> This is now a link! </h1>
+          <h2> This is now a link! </h2>
+          <h3> This is now a link! </h3>
     </div>
   );
 }
