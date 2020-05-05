8e3f66658645b9111ace698f6d3274f6287524da
Somi Jaiswal <42111833+somijaiswal23@users.noreply.github.com>
2020-05-05 23:39:33 +1000

    updated

--- src/components/demoWithStyled/index.js
+++ src/components/demoWithStyled/index.js
@@ -4,10 +4,10 @@ import React from 'react';
 function DemoWithStyled() {
   return (
     <div>
-         <Button >I am a Button</Button>
-          <Button >I am a Button</Button>
-           <Button >I am a Button</Button>
-           <Button>I am a Button</Button>
+         <button >I am a Button</button>
+          <button >I am a Button</button>
+           <button >I am a Button</button>
+           <button>I am a Button</button>
            <h1> This is now a link! </h1>
           <h2> This is now a link! </h2>
           <h3> This is now a link! </h3>
