6e608daf63e8ea5c2e740e55d59e72a7fa6d38be
Somi Jaiswal <42111833+somijaiswal23@users.noreply.github.com>
2020-05-06 23:39:35 +1000

    Update class

--- src/components/demoWithCss/index.js
+++ src/components/demoWithCss/index.js
@@ -4,10 +4,10 @@ import './style.css';
 function DemoWithCss() {
   return (
     <div>
-         <button className="btn btn-primary">I am a Button</button>
+         <button className="btn btn-danger">I am a Button</button>
           <button className="btn btn-success">I am a Button</button>
            <button className="btn btn-warning">I am a Button</button>
-           <button className="btn btn-primary font30">I am a Button</button>
+           <button className="btn btn-danger font30">I am a Button</button>
            <h1> This is now a link! </h1>
           <h2> This is now a link! </h2>
           <h3> This is now a link! </h3>
