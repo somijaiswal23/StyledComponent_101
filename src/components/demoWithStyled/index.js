21e5c8e7c14177a751aa2aa9d4635ab3a974f950
Somi Jaiswal <42111833+somijaiswal23@users.noreply.github.com>
2020-05-08 23:22:48 +1000

    Using Styled Components

--- src/components/demoWithStyled/index.js
+++ src/components/demoWithStyled/index.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  border: ${props =>
+    props.success
+      ? "2px solid green"
+      : props.warning
+      ? "2px solid orange"
+      : props.danger
+      ? "2px solid red"
+      : "2px solid pink"};
+  color: ${props =>
+    props.success
+      ? "green"
+      : props.warning
+      ? "orange"
+      : props.danger
+      ? "red"
+      : "pink"};
+  &:hover {
+    background-color: ${props =>
+      props.success
+        ? "green"
+        : props.warning
+        ? "orange"
+        : props.danger
+        ? "red"
+        : "pink"};
+     color:white;
+  }
+`;
+const Header = styled.h1`color: red`;
+
+const NewButton = styled(Button)`font-size: 30px;`;
 
 function DemoWithStyled() {
   return (
     <div>
-         <button >I am a Button</button>
-          <button >I am a Button</button>
-           <button >I am a Button</button>
-           <button>I am a Button</button>
-           <h1> This is now a link! </h1>
-          <h2> This is now a link! </h2>
-          <h3> This is now a link! </h3>
+      <Button danger>I am a Button</Button>
+      <Button success>I am a Button</Button>
+      <Button warning>I am a Button</Button>
+      <NewButton>I am a Button</NewButton>
+      <Header> This is now a link! </Header>
+      <Header as="h2"> This is now a link! </Header>
+      <Header as="h3"> This is now a link! </Header>
     </div>
   );
 }
